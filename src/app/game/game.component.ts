465cb31a8c863d3b7ef259a6c6f0c169496a493d
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-12-12 06:34:10 +0100

    added an object (json) / model for saving informations

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,6 +11,13 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game!: Game;    // "!" hier noch notwendig, da Variable noch nicht initalisiert wurde
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
 
   takeCard() {
     this.pickCardAnimation = true;
