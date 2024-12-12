4f2ad67cf9f08dc794a99c4c59f257167e192b0b
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-12-12 06:40:28 +0100

    fixed for-loop in game.ts

--- src/models/game.ts
+++ src/models/game.ts
@@ -6,10 +6,10 @@ export class Game {
 
     constructor() {
         for (let i = 1; i < 14; i++) {
-            this.stack.push('ace_' + 1);
-            this.stack.push('clubs_' + 1);
-            this.stack.push('diamonds_' + 1);
-            this.stack.push('hearts_' + 1);
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
         }
 
         shuffle(this.stack);
