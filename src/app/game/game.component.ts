7d2554031babca2fdfc300df08eac2f4316dda8e
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2024-12-13 06:40:33 +0100

    added function to show the right card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -11,16 +11,32 @@ import { Game } from '../../models/game';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard: string = '';
   game!: Game;    // "!" hier noch notwendig, da Variable noch nicht initalisiert wurde
+
+  constructor() {
+    this.newGame();
+  }
 
   newGame() {
     this.game = new Game();
     console.log(this.game);
-    
+
   }
 
   takeCard() {
+    // this.currentCard = this.game.stack.pop(); => hier kommt Fehlermeldung vermutlich wegen neuerer Angular-Version
+
+    // Alternative vom Modul 13 - TypeScript Grundkurs, 09 - Tipps und Tricks (pop() Funktion, if() Abfrage nutzen)
+    let card = this.game.stack.pop();
+
+    if (card != undefined) {
+      this.currentCard = card;
+    } else {
+      card
+    }
+
+    console.log(this.currentCard);
     this.pickCardAnimation = true;
   }
-
 }
