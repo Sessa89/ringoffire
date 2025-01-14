7be1d24095f2bb9f20067f59a8517b7c815a30af
Philip Baumgärtner <philip_baumgaertner@hotmail.de>
2025-01-14 21:39:59 +0100

    installing and connecting with firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "ring-of-fire-66811",
+      "appId": "1:624656755483:web:54e4dcf835a05161f301be",
+      "storageBucket": "ring-of-fire-66811.firebasestorage.app",
+      "apiKey": "AIzaSyAckQV1jIlDUDeQF_kZnrV2NS2_pRX7MhE",
+      "authDomain": "ring-of-fire-66811.firebaseapp.com",
+      "messagingSenderId": "624656755483"
+    })),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
+  ]
 };
